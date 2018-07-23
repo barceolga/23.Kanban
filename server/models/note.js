b8ea90be89b065d35f935b91c7102d815e49241b
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-23 17:25:46 +0200

    Create a new model note.js

--- server/models/note.js
+++ server/models/note.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
+  task: { type: 'String', required: true },
+  id: { type: 'String', required: true, unique: true },
 
 });
 
