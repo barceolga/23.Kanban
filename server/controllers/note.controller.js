c330e4d68a5318ab86a335281c214f11d8d1fcfb
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-24 09:46:18 +0200

    Add 2 new functions to note.controller.js file

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -28,3 +28,40 @@ export function addNote(req, res) {
       });
   });
 }
+
+export function deleteNote(req, res) {
+  const noteId = req.params.noteId;
+  Note.findOne({ id: noteId }).exec((err, note) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+      Lane.findOne({ id: req.body.laneId }).exec((err, lane) => {
+        const updatedNotes = lane.notes.filter(note => note.id !== noteId);
+        lane.notes = updatedNotes;
+        lane.save(() => {
+          note.remove(() => {
+            res.status(200).end();
+          });
+        });
+      });
+    });
+}
+
+export function editNote(req, res) {
+  const noteId = req.params.noteId;
+  const newTask = req.body.task;
+  Note.findOne({ id: noteId }).exec((err, note) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    note.task = newTask;
+    note.save((err, noteSaved) => {
+      if (err) {
+        res.status(500).end();
+      }
+      res.json(noteSaved);
+    });
+  });
+}
