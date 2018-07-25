fdf1a828dc00426bb49ad982a049ee1fdc6e0fe2
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-25 16:01:50 +0200

    Update content for NoteActions.js file

--- client/modules/Note/NoteActions.js
+++ client/modules/Note/NoteActions.js
@@ -1,9 +1,13 @@
-import uuid from 'uuid';
+import callApi from '../../util/apiCaller';
+//import { lanes } from '../../util/schema';
+//import { normalize } from 'normalizr';
 
 // Export Constants
 export const CREATE_NOTE = 'CREATE_NOTE';
+export const CREATE_NOTES = 'CREATE_NOTES';
 export const UPDATE_NOTE = 'UPDATE_NOTE';
 export const DELETE_NOTE = 'DELETE_NOTE';
+export const EDIT_NOTE = 'EDIT_NOTE';
 
 // Export Actions
 export function createNote(note, laneId) {
@@ -11,12 +15,25 @@ export function createNote(note, laneId) {
     type: CREATE_NOTE,
     laneId,
     note: {
-      id: uuid(),
+      laneId: laneId,
       ...note,
     },
   };
 }
 
+export function createNotes(notesData) {
+  return {
+    type: CREATE_NOTES,
+    notes: notesData,
+  }
+}
+export function createNoteRequest(note, laneId) {
+  return (dispatch) => {
+    return callApi('notes', 'post', {note, laneId }.then(noteResp => {
+      dispatch(createNote(noteResp, laneId));
+    }))
+  }
+}
 export function updateNote(note) {
   return {
     type: UPDATE_NOTE,
@@ -27,6 +44,14 @@ export function updateNote(note) {
 export function deleteNote(noteId, laneId) {
   return {
     type: DELETE_NOTE,
+    noteId,
+    laneId,
+  };
+}
+
+export function editNote(noteId, laneId) {
+  return {
+    type: EDIT_NOTE,
     noteId,
     laneId,
   };
