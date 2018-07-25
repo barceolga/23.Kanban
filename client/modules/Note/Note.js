475c235118ca293b1301e7215ad71f414635db5b
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-25 16:01:09 +0200

    Update content for Note.js file

--- client/modules/Note/Note.js
+++ client/modules/Note/Note.js
@@ -1,10 +1,11 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // Import Style
 import styles from './Note.css';
 
 const Note = props =>
-    <li className={styles.Note}>{props.children}</li>
+    <li className={styles.Note}>{props.children}</li>;
 
 Note.propTypes = {
     children: PropTypes.any,
