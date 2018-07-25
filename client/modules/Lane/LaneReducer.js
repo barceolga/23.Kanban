6c9ab5311c7d2ac838de6bbd9cc0c0643f2a6df9
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-25 15:59:11 +0200

    Update content for LaneReducer.js

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,42 +1,34 @@
 // Import Actions
-import {  CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import {  CREATE_LANE, CREATE_LANES, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
 import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions.js';
+
+import omit from 'lodash/omit';
 
 // Initial State
 const initialState = [];
 
-export default function lanes(state = initialState, action){
+export default function lanes(state = initialState, action) {
   switch (action.type) {
     case CREATE_LANE:
-        return [...state, action.lane];
     case UPDATE_LANE:
-        return state.map(lane => {
-          if (lane.id === action.id) {
-            return Object.assign({}, lane, action.lane);
-          }
-
-          return lane;
-        });
+          return { ...state, [action.lane.id]: action.lane };
+    case CREATE_LANES:
+        return { ...action.lanes};
     case DELETE_LANE:
-        return state.filter(lane => lane.id !==action.laneId)
+        return omit(state, action.laneId);
     case CREATE_NOTE: {
-        return state.map(lane => {
-          if (lane.id === action.laneId) {
-            const notes = [...lane.notes, action.note.id];
-            return {...lane, notes};
-          }
-          return lane;
-        })
+        const newLane = { ...state[action.laneId] };
+        newLane.notes = newLane.notes.concat(action.note.id);
+        return { ...state, [action.laneId]: newLane };
     }
-    case DELETE_NOTE: {
-        return state.filter(lane => {
-          if (lane.id !== action.laneId) {
-            const notes = [...lane.notes, action.note.id];
-            return {...lane, notes}
-          }
-          return lane;
-        })
-    }
+    case DELETE_NOTE:
+        const newLane = {...state[action.laneId] };
+        newLane.notes = newLane.notes.filter(noteId => noteId !== action.noteId);
+
+        return {...state, [action.laneId]: newLane };
+    case EDIT_LANE:
+      const lane = { ...state[action.id], editing: true };
+      return {...state, [action.id]: lane };
     default:
       return state;
   }
