fa513cf2a7f0aab44c3c982ef3de1250821c9659
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-25 16:03:01 +0200

    Update content for Notes.js file

--- client/modules/Note/Notes.js
+++ client/modules/Note/Notes.js
@@ -1,19 +1,38 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Note from './Note';
+import Edit from '../../components/Edit.js';
+import { editNote, updateNote, deleteNote } from './NoteActions.js';
 import styles from './Notes.css';
 
-const Notes = ({ notes }) => {
+const Notes = ({ notes, laneId, editNote, updateNote, deleteNote }) => {
   return (<ul className='notes'>{notes.map((note) =>
     <Note
       id={note.id}
       key={note.id}
+      editing={note.editing}
       >
-      {note.task}
+        <Edit
+          editing={note.editing}
+          value={note.task}
+          onValueClick={() => editNote(note.id)}
+          onUpdate={(task) => updateNOte({
+            ...note,
+            taks,
+            editing: false,
+          }
+        )}
+        onDelete={() => deleteNote(note.id, laneId)}
+        />
       </Note>
   )}</ul>);
 };
 
 Notes.propTypes = {
+  deleteNote: PropTypes.func,
+  updateNote: PropTypes.func,
+  editNote: PropTypes.func,
+  laneId: PropTypes.string,
   notes: PropTypes.array,
 };
 
