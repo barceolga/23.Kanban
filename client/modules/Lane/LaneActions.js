6fd521fa3e37250c0bfed34fa4acf6be6ff380a9
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-25 15:58:34 +0200

    Add new types of actions and action's creators into LaneAction.js file

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,10 +1,15 @@
-import uuid from 'uuid';
+import callApi from '../../util/apiCaller';
+import { lanes } from '../../util/schema';
+import { normalize } from 'normalizr';
+import { createNotes } from '../Note/NoteActions';
 
 // Export Constants
 
 export const CREATE_LANE = 'CREATE_LANE';
+export const CREATE_LANES = 'CREATE_LANES';
 export const UPDATE_LANE = 'UPDATE_LANE';
 export const DELETE_LANE = 'DELETE_LANE';
+export const EDIT_LANE = 'EDIT_LANE';
 
 // Export Actions
 
@@ -12,11 +17,25 @@ export function createLane(lane) {
   return {
     type: CREATE_LANE,
     lane: {
-      id: uuid(),
       notes: [],
       ...lane,
     }
   };
+}
+
+export function createLaneRequest(lane) {
+  return (dispatch) => {
+    return callApi('lanes', 'post', lane).then(res => {
+      dispatch(createLane(res));
+    })
+  }
+}
+
+export function createLanes(lanesData) {
+  return {
+    type: CREATE_LANES,
+    lanes: lanesData,
+  }
 }
 
 export function updateLane(lane) {
@@ -31,4 +50,22 @@ export function deleteLane(laneId) {
     type: DELETE_LANE,
     laneId,
   };
+}
+
+export function editLane(laneId) {
+  return {
+    type: EDIT_LANE,
+    laneId,
+  };
+}
+
+export function fetchLanes() {
+  return (dispatch) => {
+    return callApi('lanes').then(res => {
+      const normalized = normalize(res.lanes, lanes);
+      const { lanes: normalizedLanes, notes } = normalized.entities;
+      dispatch(createLanes(normalizedlanes));
+      dispatch(createNotes(notes));
+    })
+  }
 }
