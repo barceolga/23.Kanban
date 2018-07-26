cf42d25734343f00a885ee802c37435b394dd333
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-26 16:37:08 +0200

    Update content for NoteContainer.js file

--- client/modules/Note/NotesContainer.js
+++ client/modules/Note/NotesContainer.js
@@ -1,11 +1,11 @@
 import { connect } from 'react-redux';
 import Notes from './Notes';
-import { updateNote, editNote, deleteNote, deleteNoteRequest } from '../Note/NoteActions';
+import { updateNote, editNote, deleteNote, deleteNoteRequest, updateNoteRequest } from '../Note/NoteActions';
 
 const mapDispatchToProps = {
-  updateNote,
-  editNote,
+  updateNote: updateNoteRequest,
   deleteNote: deleteNoteRequest,
+  editNote,
 };
 
 export default connect(
