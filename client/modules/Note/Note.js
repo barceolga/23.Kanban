dd5d9744eaf265d6f31cd86732883d796023a4a6
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-27 19:51:52 +0200

    Update Note.js content (drag and drop)

--- client/modules/Note/Note.js
+++ client/modules/Note/Note.js
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {DragSource, DropTarget } from 'react-dnd';
+import {compose} from 'redux';
+import ItemTypes from '../Kanban/itemTypes';
 
 // Import Style
 import styles from './Note.css';
 
-const Note = props =>
-    <li className={styles.Note}>{props.children}</li>;
 
-Note.propTypes = {
-    children: PropTypes.any,
-};
+class Note extends React.Component {
+  constructor(props) {
+    super(props);
+      this.props= props;
+  }
+  render() {
+    const {
+      connectDragSource,
+      connectDropTarget,
+      isDragging,
+      editing,
+      children
+    } = this.props;
 
-export default Note;
+// If the note is beeing edited, it cannot be dragged, therefore the condition set below:
+      const dragSource = editing ? a => a : connectDragSource;
+
+      return dragSource(connectDropTarget(
+        <li className={styles.Note}
+        style={{
+          opacity: isDragging ? 0 : 1,
+        }}
+        >
+        {children}
+        </li>
+      ));
+  }
+}
+
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      laneId: props.laneId,
+    };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
+  }
+}
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+
+    if (targetProps.id !== sourceProps.id) {
+      targetProps.moveWithinLane(targetProps.laneId, targetProps.id, sourceProps.id);
+    }
+  }
+}
+export default compose(
+  DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  })),
+  DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget(),
+  }))
+) (Note)
