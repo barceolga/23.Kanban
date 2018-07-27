2b1c242bd5e3c1c0ba6513eeaa4f863cbfab7e47
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-27 11:56:50 +0200

    Update content and fix bugs in LaneReducer.js file

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,6 @@
+
 // Import Actions
-import {  CREATE_LANE, CREATE_LANES, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
+import { CREATE_LANE, CREATE_LANES, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
 import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions.js';
 
 import omit from 'lodash/omit';
@@ -13,23 +14,21 @@ export default function lanes(state = initialState, action) {
     case UPDATE_LANE:
           return { ...state, [action.lane.id]: action.lane };
     case CREATE_LANES:
-        return { ...action.lanes};
+        return { ...action.lanes };
     case DELETE_LANE:
         return omit(state, action.laneId);
-    case CREATE_NOTE: {
+    case CREATE_NOTE:
         const newLane = { ...state[action.laneId] };
         newLane.notes = newLane.notes.concat(action.note.id);
         return { ...state, [action.laneId]: newLane };
-    }
     case DELETE_NOTE:
-        const newLane = { ...state[action.laneId] };
-        newLane.notes = newLane.notes.filter(noteId => noteId !== action.noteId);
-
-        return {...state, [action.laneId]: newLane };
+        const updatedLane = { ...state[action.laneId] };
+        updatedLane.notes = updatedLane.notes.filter(noteId => noteId !== action.noteId);
+        return { ...state, [action.laneId]: updatedLane };
     case EDIT_LANE:
-      const lane = { ...state[action.id], editing: true };
-      return {...state, [action.id]: lane };
+      const lane = { ...state[action.laneId], editing: true };
+      return { ...state, [action.laneId]: lane };
     default:
       return state;
   }
-};
+}
