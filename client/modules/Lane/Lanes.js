66bd135af7b6171cbf602cf3478f56a8e2a479cf
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-27 11:57:16 +0200

    Update content for Lanes.js file

--- client/modules/Lane/Lanes.js
+++ client/modules/Lane/Lanes.js
@@ -4,7 +4,6 @@ import Lane from './LaneContainer.js';
 import styles from './Lanes.css';
 
 const Lanes = ({ lanes }) => {
-
   return (
       <div className={styles.lanes}>{lanes.map(lane =>
         <Lane className="lane" key={lane.id} lane={lane} />
