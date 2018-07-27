1aad2846d73f09ab358b5fa55d5b79f38795d5e1
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-27 12:01:55 +0200

    Update content for lane.route.js file

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,16 +3,16 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
-//Get all lanes
+// Get all lanes
 router.route('/lanes').get(LaneController.getLanes);
 
-//Add a new Lane
+// Add a new Lane
 router.route('/lanes').post(LaneController.addLane);
 
-//Delete a lane
+// Delete a lane
 router.route('/lanes/:laneId').delete(LaneController.deleteLane);
 
-//Edit lane name
+// Edit lane name
 router.route('/lanes/:laneId').put(LaneController.editLaneName);
 
 export default router;
