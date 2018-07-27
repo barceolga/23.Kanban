3105006cca2db226d3d44738ae67465150546c77
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-27 11:22:39 +0200

    Update content and fix bugs for Lane.js

--- client/modules/Lane/Lane.js
+++ client/modules/Lane/Lane.js
@@ -5,23 +5,23 @@ import Edit from '../../components/Edit.js';
 // Import Style
 import styles from './Lane.css';
 
-const Lane = (props) => {
+const Lane = props => {
 const { lane, laneNotes, updateLane, addNote, editLane, deleteLane } = props;
 const laneId = lane.id;
 
     return (
-      <div className={styles.Lane} key={laneId}>
+      <div className={styles.Lane}>
           <div className={styles.LaneHeader}>
               <div className={styles.LaneAddNote}>
-                  <button onClick={() => addNote({ task: 'New Note'}, laneId )}>Add Note</button>
+                  <button onClick={() => addNote({ task: 'New Note' }, laneId)}>Add Note</button>
               </div>
               <Edit
                     className={styles.LaneName}
                     editing={lane.editing}
                     value={lane.name}
-                    onValueClick={() => editLane(laneId)}
+                    onValueClick={() => editLane(lane.id)}
 
-                    onUpdate={name => updateLane({...lane, name, editing: false})}
+                    onUpdate={name => updateLane({ ...lane, name, editing: false })}
               />
               <div className={styles.LaneDelete}>
                   <button onClick={() => deleteLane(laneId)}>Remove Lane</button>
@@ -42,6 +42,7 @@ Lane.propTypes = {
   addNote: PropTypes.func,
   updateLane: PropTypes.func,
   deleteLane: PropTypes.func,
+  editLane: PropTypes.func,
 };
 
 export default Lane;
