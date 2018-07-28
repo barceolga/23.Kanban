60a9f522ed2254a58a36ec1d7a8c981228aae6f2
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-28 18:49:37 +0200

    Update Lanes.js file

--- client/modules/Lane/Lanes.js
+++ client/modules/Lane/Lanes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Lane from './LaneContainer.js';
+import { moveBetweenLanes } from './LaneActions';
+import { moveLanes } from '../Kanban/KanbanActions';
 import styles from './Lanes.css';
 
-const Lanes = ({ lanes }) => {
+const Lanes = ({ lanes, moveNotes, moveBetweenLanes }) => {
   return (
       <div className={styles.lanes}>{lanes.map(lane =>
-        <Lane className="lane" key={lane.id} lane={lane} />
+        <Lane className="lane" key={lane.id} lane={lane} moveNotes={moveNotes} moveBetweenLanes={moveBetweenLanes} />
       )}</div>
   );
 };
