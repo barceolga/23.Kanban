4e4a8a22e77bc3e31438aef420e1a402458ff583
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-28 18:51:01 +0200

    Add new schema kanban to schema.js file

--- client/util/schema.js
+++ client/util/schema.js
@@ -4,5 +4,8 @@ const note = new schema.Entity('notes');
 const lane = new schema.Entity('lanes', {
   notes: [note],
 });
+const kanban = new schema.Entity('kanban', {
+  lanes: [lane],
+})
 
 export const lanes = [lane];
