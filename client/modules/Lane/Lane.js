98c173b96cf84b484e942dd58bda13b2cc9c22ff
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-28 18:47:58 +0200

    Update Lane.js file

--- client/modules/Lane/Lane.js
+++ client/modules/Lane/Lane.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import NotesContainer from '../Note/NotesContainer';
 import Edit from '../../components/Edit.js';
+import ItemTypes from '../Kanban/itemTypes';
+import {DragSource, DropTarget } from 'react-dnd';
+import {compose} from 'redux';
+
 // Import Style
 import styles from './Lane.css';
 
@@ -11,44 +15,74 @@ class Lane extends React.Component {
       this.props = props;
   }
 render() {
+
   const {
+    connectDragSource,
     connectDropTarget,
+    isDragging,
     lane,
     laneNotes,
     updateLane,
     addNote,
     editLane,
-    deleteLane
+    deleteLane,
+    editing,
   } = this.props;
 
   const laneId = lane.id;
-  return connectDropTarget (
-      <div className={styles.Lane}>
-          <div className={styles.LaneHeader}>
-              <div className={styles.LaneAddNote}>
-                  <button onClick={() => addNote({ task: 'New Note' }, laneId)}>Add Note</button>
-              </div>
-              <Edit
-                    className={styles.LaneName}
-                    editing={lane.editing}
-                    value={lane.name}
-                    onValueClick={() => editLane(lane.id)}
 
-                    onUpdate={name => updateLane({ ...lane, name, editing: false })}
-              />
-              <div className={styles.LaneDelete}>
-                  <button onClick={() => deleteLane(laneId)}>Remove Lane</button>
-              </div>
+  const dragSource = editing ? a => a : connectDragSource;
+
+  return dragSource(connectDropTarget(
+
+      <div className={styles.Lane}
+      style={{
+        opacity: isDragging ? 0 : 1,
+      }}>
+      <div className={styles.LaneHeader}>
+          <div className={styles.LaneAddNote}>
+              <button onClick={() => addNote({ task: 'New Note' }, laneId)}>Add Note</button>
           </div>
-          <NotesContainer
-            notes={laneNotes}
-            laneId={laneId}
-          />
-      </div>
-    );
-  }
-};
+          <Edit
+                className={styles.LaneName}
+                editing={lane.editing}
+                value={lane.name}
+                onValueClick={() => editLane(lane.id)}
 
+                onUpdate={name => updateLane({ ...lane, name, editing: false })}
+          />
+          <div className={styles.LaneDelete}>
+              <button onClick={() => deleteLane(laneId)}>Remove Lane</button>
+          </div>
+      </div>
+      <NotesContainer
+        notes={laneNotes}
+        laneId={laneId}
+      />
+      </div>
+    ));
+  }
+}
+
+const laneSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+    };
+},
+isDragging(props, monitor) {
+  return props.id === monitor.getItem().id;
+}
+}
+
+const laneTarget = {
+hover(targetProps, monitor) {
+  const sourceProps = monitor.getItem();
+    if (targetProps.id !== sourceProps.id) {
+      targetProps.moveLane(targetProps.id, sourceProps.id);
+    }
+  }
+}
 
 Lane.propTypes = {
   lane: PropTypes.object,
@@ -57,6 +91,15 @@ Lane.propTypes = {
   updateLane: PropTypes.func,
   deleteLane: PropTypes.func,
   editLane: PropTypes.func,
+  moveLane:PropTypes.func,
 };
 
-export default Lane;
+export default compose(
+  DragSource(ItemTypes.LANE, laneSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  })),
+  DropTarget(ItemTypes.LANE, laneTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget(),
+  }))
+)(Lane);
