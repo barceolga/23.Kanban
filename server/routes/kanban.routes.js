3d2ae47654ea58c1cf2c3744344b0bd2cadd2a92
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-30 13:22:04 +0200

    Delete kanban.routes.js

--- server/routes/kanban.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as KanbanController from '../controllers/kanban.controller';
-
-const router = new Router();
-
-router.route('/kanban').post(KanbanController.setKanban);
-
-
-export default router;
