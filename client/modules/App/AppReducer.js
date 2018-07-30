e9f2d6c6d054cc26a687e80506a202385ae0994a
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-30 12:18:31 +0200

    Update content for App.Reducer.js

--- client/modules/App/AppReducer.js
+++ client/modules/App/AppReducer.js
@@ -1,16 +1,16 @@
 // Import Actions
-import { TOGGLE_ADD_POST } from './AppActions';
+import { TOGGLE_ADD_BOARD} from './AppActions';
 
 // Initial State
 const initialState = {
-  showAddPost: false,
+  showAddBoard: false,
 };
 
 const AppReducer = (state = initialState, action) => {
   switch (action.type) {
-    case TOGGLE_ADD_POST:
+    case TOGGLE_ADD_BOARD:
       return {
-        showAddPost: !state.showAddPost,
+        showAddBoard: !state.showAddBoard,
       };
 
     default:
@@ -20,8 +20,8 @@ const AppReducer = (state = initialState, action) => {
 
 /* Selectors */
 
-// Get showAddPost
-export const getShowAddPost = state => state.app.showAddPost;
+// Get showAddBoard
+export const getShowAddBoard = state => state.app.showAddBoard;
 
 // Export Reducer
 export default AppReducer;
