240b08f221beb26422d172a0a5e79033911b906e
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-30 13:25:14 +0200

    Add board.controller.js file

---
+++ server/controllers/board.controller.js
@@ -0,0 +1,29 @@
+import Board from '../models/board';
+import Lane from '../models/lane';
+import uuid from 'uuid';
+
+
+export function setBoard(req, res) {
+  if (!req.body) {
+    res.status(403).end();
+  }
+  const newBoard = new Board(req.body);
+
+  newBoard.lanes = [];
+  newBoard.id = uuid();
+  newBoard.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
+}
+
+export function getBoards(req, res) {
+  Board.find().exec((err, boards) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+      res.json({ boards });
+  });
+}
