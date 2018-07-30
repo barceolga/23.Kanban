5986d6da9342f19a09d1629abace38dfdc6d1bb1
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-30 13:13:36 +0200

    Add new action and fix bugs in KanbanActions.js file

--- client/modules/Kanban/KanbanActions.js
+++ client/modules/Kanban/KanbanActions.js
@@ -1,10 +1,53 @@
+import callApi from '../../util/apiCaller';
+import { boards } from '../../util/schema';
+import { normalize } from 'normalizr';
+import { createLanes } from '../Lane/LaneActions';
 
+export const ADD_BOARD = 'ADD_BOARD';
+export const CREATE_BOARDS = 'CREATE_BOARDS';
 export const MOVE_LANE = 'MOVE_LANE';
 
-export function moveLane(targetId, sourceId) {
+export function addBoard(board) {
+  return {
+    type: ADD_BOARD,
+    board: {
+      lanes: [],
+      ...board,
+    },
+  };
+}
+
+export function addBoardRequest(lane) {
+  return (dispatch) => {
+    return callApi('boards', 'post', board).then(res => {
+      dispatch(addBoard(res));
+    });
+  };
+}
+
+export function createBoards(boardsData) {
+  return {
+    type: CREATE_BOARDS,
+    boards: boardsData,
+  };
+}
+
+export function moveLane(boardId, targetId, sourceId) {
   return  {
     type: MOVE_LANE,
+    boardId,
     sourceId,
     targetId,
+  };
+}
+
+export function fetchBoards() {
+  return (dispatch) => {
+    return callApi('boards').then(res => {
+      const normalized = normalize(res.boards, boards);
+      const { boards: normalizedBoards, lanes } = normalized.entities;
+      dispatch(createBoards(normalizedBoards));
+      dispatch(createLanes(lanes));
+    });
   };
 }
