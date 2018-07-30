87712ded3199eacab83dbb87f32c5c37ce4aacc7
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-30 13:17:36 +0200

    Update routes.js file

--- client/routes.js
+++ client/routes.js
@@ -16,6 +16,7 @@ if (typeof require.ensure !== 'function') {
  */
 if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
+  require('./modules/Post/pages/BoardPage/BoardPage');
   require('./modules/Kanban/Kanban');
 }
 
@@ -26,9 +27,17 @@ export default (
     <IndexRoute
       getComponent={(nextState, cb) => {
         require.ensure([], require => {
-          cb(null, require('./modules/Kanban/Kanban').default);
+          cb(null, require('./modules/Post/pages/BoardPage/BoardPage').default);
         });
       }}
     />
+    <Route
+    path="/board"
+    getComponent={(nextState, cb) => {
+      require.ensure([], require => {
+        cb(null, require('./modules/Kanban/Kanban').default);
+      });
+    }}
+  />
   </Route>
 );
