387c125efdbe978b8dac5b46af2e2effeadebd35
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-30 12:18:03 +0200

    Add new action type and action creator in AppActions.js

--- client/modules/App/AppActions.js
+++ client/modules/App/AppActions.js
@@ -1,9 +1,10 @@
 // Export Constants
-export const TOGGLE_ADD_POST = 'TOGGLE_ADD_POST';
+export const TOGGLE_ADD_BOARD = 'TOGGLE_ADD_BOARD';
 
 // Export Actions
-export function toggleAddPost() {
+
+export function toggleAddBoard() {
   return {
-    type: TOGGLE_ADD_POST,
+    type: TOGGLE_ADD_BOARD,
   };
 }
